211ad846f2985e0cf6562cd9140848d45171ecac
priya <n_priyahere@yahoo.com>
2021-05-09 06:05:31 +0000

    conversion committed

---
+++ src/OOPS/Conversion.java
@@ -0,0 +1,18 @@
+package OOPS;
+
+public class Conversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char b = 'b';
+		System.out.println("Enter the character " + b);
+		
+		double num = 5.6;
+		int num1 = (int) num;
+		
+		System.out.println(num1);
+
+	}
+
+}
